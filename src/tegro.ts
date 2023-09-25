6c44314fbeb069c2d5950cb45599c52cd1092613
ashish <ashish@tegro.com>
2023-09-25 17:31:39 +0400

    add daily and weekly data

--- src/tegro.ts
+++ src/tegro.ts
@@ -2,7 +2,7 @@ import {
   TradeFailed as TradeFailedEvent,
   TradeSuccessful as TradeSuccessfulEvent
 } from "../generated/tegro/tegro"
-import { TradeFailed, TradeSuccessful, TotalVolume } from "../generated/schema"
+import { TradeFailed, TradeSuccessful, TotalVolume, DailyVolume, WeeklyVolume } from "../generated/schema"
 import { BigInt, BigDecimal, log } from "@graphprotocol/graph-ts";
 
 const USDT_ADDRESS = "0xdac17f958d2ee523a2206206994597c13d831ec7";
@@ -39,28 +39,59 @@ export function handleTradeSuccessful(event: TradeSuccessfulEvent): void {
   entity.save()
 
 
-   // If the trade involves USDT as makerAsset or takerAsset, update the total volume
-   let usdtVolume: BigDecimal;
-   log.info("Maker Asset: {}", [entity.makerAsset.toHexString()]);
-   log.info("Taker Asset: {}", [entity.takerAsset.toHexString()]); 
-   
-   if (entity.makerAsset.toHexString() == USDT_ADDRESS) {
-     usdtVolume = toHumanReadable(entity.makerAmount);
-   } else if (entity.takerAsset.toHexString() == USDT_ADDRESS) {
-     usdtVolume = toHumanReadable(entity.takerAmount);
-   } else {
-     return; // If neither the maker nor taker asset is USDT, do not proceed
-   }
+  // If the trade involves USDT as makerAsset or takerAsset, update the total volume
+  let usdtVolume: BigDecimal;
+  log.info("Maker Asset: {}", [entity.makerAsset.toHexString()]);
+  log.info("Taker Asset: {}", [entity.takerAsset.toHexString()]);
 
-   let volumeEntity = TotalVolume.load("usdt_volume");
- 
-   if (volumeEntity == null) {
-     volumeEntity = new TotalVolume("usdt_volume");
-     volumeEntity.volume = BigDecimal.fromString("0");
-    }
- 
-   volumeEntity.volume = volumeEntity.volume.plus(usdtVolume);
-   volumeEntity.save();
+  if (entity.makerAsset.toHexString() == USDT_ADDRESS) {
+    usdtVolume = toHumanReadable(entity.makerAmount);
+  } else if (entity.takerAsset.toHexString() == USDT_ADDRESS) {
+    usdtVolume = toHumanReadable(entity.takerAmount);
+  } else {
+    return; // If neither the maker nor taker asset is USDT, do not proceed
+  }
+
+  let volumeEntity = TotalVolume.load("usdt_volume");
+
+  if (volumeEntity == null) {
+    volumeEntity = new TotalVolume("usdt_volume");
+    volumeEntity.volume = BigDecimal.fromString("0");
+  }
+
+  volumeEntity.volume = volumeEntity.volume.plus(usdtVolume);
+  volumeEntity.save();
+
+  // Convert the block timestamp to date and week number
+  let timestamp = event.block.timestamp;
+  let date = timestamp.toI32() / 86400; // convert timestamp to days since epoch
+  let week = date / 7;
+
+  // Handle daily volume
+  let dailyVolumeId = date.toString();
+  let dailyVolume = DailyVolume.load(dailyVolumeId);
+
+  if (dailyVolume == null) {
+    dailyVolume = new DailyVolume(dailyVolumeId);
+    dailyVolume.date = date;
+    dailyVolume.volume = BigDecimal.fromString("0");
+  }
+
+  dailyVolume.volume = dailyVolume.volume.plus(usdtVolume);
+  dailyVolume.save();
+
+  // Handle weekly volume
+  let weeklyVolumeId = week.toString();
+  let weeklyVolume = WeeklyVolume.load(weeklyVolumeId);
+
+  if (weeklyVolume == null) {
+    weeklyVolume = new WeeklyVolume(weeklyVolumeId);
+    weeklyVolume.week = week;
+    weeklyVolume.volume = BigDecimal.fromString("0");
+  }
+
+  weeklyVolume.volume = weeklyVolume.volume.plus(usdtVolume);
+  weeklyVolume.save();
 }
 
 function toHumanReadable(amount: BigInt): BigDecimal {
